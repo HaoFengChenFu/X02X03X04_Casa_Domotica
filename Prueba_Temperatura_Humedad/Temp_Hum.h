cc72828a91cebcae919e027d40a5f839e4358212
Hao <feng.chen.fu@alumnos.upm.es>
2023-03-28 11:55:00 +0200

    Version funcional

--- Prueba_Temperatura_Humedad/Temp_Hum.h
+++ Prueba_Temperatura_Humedad/Temp_Hum.h
@@ -2,8 +2,11 @@
 #define __TemperaturaHumedad_H
 
 	#define ADDR_TEMP_HUM 0x45
-	#define CMD_PREIODIC_ADQ_MSB 0x2C
-	#define CMD_PREIODIC_ADQ_LSB 0x06
+	#define CMD_SINGLE_ADQ_MSB 0x2C
+	#define CMD_SINGLE_ADQ_LSB 0x06
+	
+	#define CMD_PERIODIC_ADQ_MSB 0x21
+	#define CMD_PERIODIC_ADQ_LSB 0x26
 	
 	typedef struct
 	{
@@ -15,5 +18,11 @@
 	
 	int Init_ThTemp_Hum (void);
 	int Init_MsgTemp_Hum (void);
-
+	
+	void Config_Communication(void);
+	
+	void Set_SingleShot_Mode(void);
+	void Set_PeriodicAdq_Mode(void);
+	void ReadSensorData(void);
+	
 #endif
